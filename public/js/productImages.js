56a560a57515d8978d92cbab940c0b56ec90f6cf
Phattharalada Suksena <phattharaladasuksena@gmail.com>
2024-12-17 01:09:49 +0700

    update

--- public/js/productImages.js
+++ public/js/productImages.js
@@ -3,7 +3,19 @@ const productImages = {
     1: '/picture/banana1.jpg',
     2: '/picture/banana2.jpg',
     3: '/picture/banana3.jpg',
-    4: '/picture/watermelon1.jpg'
+    4: '/picture/watermelon1.jpg',
+    5: '/picture/G1_1.jpg',
+    6: '/picture/G1_2.jpg',
+    7: '/picture/G1_3.jpg',
+    8: '/picture/G2_1.jpg',
+    9: '/picture/G2_2.jpg',
+    10: '/picture/G2_3.jpg',
+    11: '/picture/G3_1.jpg',
+    12: '/picture/G3_2.jpg',
+    13: '/picture/G3_3.jpg',
+    14: '/picture/G4_1.jpg',
+    15: '/picture/G4_2.jpg',
+    16: '/picture/G4_3.jpg'
 };
 
 // Export object นี้สำหรับใช้งานในไฟล์อื่น
