db75d5e06205041ecb2cfa14689927b1e33c7170
Guide42-ho <mophone563@gmail.com>
2024-12-16 21:15:14 +0700

    edit banana2,3

--- public/js/productImages.js
+++ public/js/productImages.js
@@ -1,8 +1,8 @@
 // productImages.js
 const productImages = {
     1: '/picture/banana1.jpg',
-    2: '/picture/apple1.jpg',
-    3: '/picture/orange1.jpg',
+    2: '/picture/banana2.jpg',
+    3: '/picture/banana3.jpg',
     4: '/picture/watermelon1.jpg'
 };
 
